477e49156b2e48363da5f4f0a62504dcf0005030
alexander <alexander.k.haas@web.de>
2026-01-15 15:20:54 +0100

    Add optimization scripts fpr PS4 Task 3

---
+++ Problem Set 4/Task3_strategy_optimization.R
@@ -0,0 +1,41 @@
+############################
+# EOQ Strategy optimization
+#############################
+
+get_strategy <- function(total_laps, pit_loss, tire_deg, fuel_sens, fuel_burn, max_fuel, max_tire) {
+  
+  # A: Fuel Weight Penalty (sec/lap), D: Tire Deg (sec/lap)
+  A <- fuel_burn * fuel_sens
+  D <- tire_deg
+  
+  # 1. Unconstrained Optimum (EOQ / Andler Formula)
+  # L_opt = sqrt(2 * P / (A + D))
+  L_math <- if ((A + D) == 0) total_laps else sqrt((2 * pit_loss) / (A + D))
+  
+  # 2. Apply Hard Constraints
+  # The stint cannot be longer than the tank or tire life allows
+  L_fuel <- max_fuel / fuel_burn
+  L_limit <- min(L_math, L_fuel, max_tire)
+  
+  # 3. Discretize
+  # Floor ensures we don't accidentally exceed fuel/tire limits by rounding up
+  L_final <- max(1, floor(L_limit)) 
+  
+  # Calculate resulting strategy
+  n_stints <- ceiling(total_laps / L_final)
+  
+  return(list(
+    Opt_Laps = L_final,
+    Stints = n_stints,
+    Stops = n_stints - 1,
+    Limiter = dplyr::case_when(
+      L_limit == L_math ~ "Speed (Math Optimum)",
+      L_limit == L_fuel ~ "Fuel Capacity",
+      TRUE ~ "Tire Life"
+    )
+  ))
+}
+
+# --- Usage Example ---
+get_strategy(total_laps=53, pit_loss=20, tire_deg=0.08, fuel_sens=0.035, fuel_burn=1.8, max_fuel=110, max_tire=25)
+
